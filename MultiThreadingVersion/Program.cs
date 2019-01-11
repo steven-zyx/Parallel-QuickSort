08e90c5b510aefd401ca66500bb47ca3b2b0849b
Steven Chuh <steven_chuh@qq.com>
2019-01-11 16:00:34 +0800

    normal

--- MultiThreadingVersion/Program.cs
+++ MultiThreadingVersion/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RelevantFunction
+using System;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
 
@@ -8,7 +9,31 @@ namespace MultiThreadingVersion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = Relevant.GenerateRandomIntergers(10_000, 0, 10000);
+            array[array.Length - 1] = int.MaxValue;
+            //Sort(array, 0, array.Length - 1);
+            bool result = Relevant.VerifySequence(array);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+
+
+        static int Partition(int[] array, int lo, int hi)
+        {
+            int middleEle = array[lo];
+            int i = lo;
+            int j = hi + 1;
+            while (true)
+            {
+                while (array[++i] < middleEle) ;
+                while (array[--j] > middleEle) ;
+                if (i >= j) break;
+
+                Swap(array, i, j);
+            }
+            Swap(array, lo, j);
+            return j;
         }
 
         static void Swap(int[] array, int e1, int e2)
